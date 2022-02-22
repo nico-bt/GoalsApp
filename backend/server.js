a105cd7b19ed17b4e404b2c91a129fe3209515bb
Nico Battaglia <nico-battaglia@hotmail.com>
2022-02-22 17:47:48 -0300

    Add .json .urlenconded to enable body data and errorHandler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,19 @@
+const { urlencoded } = require("express")
 const express = require("express")
+const {errorHandler} = require("./middleware/errorMiddleware")
 const dotenv = require("dotenv").config()
 const port=process.env.PORT || 5000
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+// ROUTES
+// *************************
 app.use("/api/goals", require("./routes/goalRoutes"))
 
+app.use(errorHandler)
 
 app.listen(port, ()=>{
     console.log(`Server started on port ${port}`)
